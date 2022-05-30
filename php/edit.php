02d02c3e3f246ece129d1d74801934987fd2ac67
mackliu <mackliu@gmail.com>
2022-05-30 13:19:38 +0800

    從資料表中撈出對應id的資料並顯示在編輯表單上

--- php/edit.php
+++ php/edit.php
@@ -20,21 +20,24 @@
 <body>
     <h1>編輯學生資料</h1>
     <?php
-
         $id=$_GET['id'];
-        echo "要編輯的是id為".$id."的資料";
-?>
+        $dsn="mysql:host=localhost;charset=utf8;dbname=school";
+        $pdo=new PDO($dsn,'root','');
+        $sql="SELECT * FROM `students` WHERE `id`='$id'";
+        $student=$pdo->query($sql)->fetch();
+
+    ?>
     <form action="update.php" method="post">
-        <label for="">學號：<input type="text" name="uni_id" id=""></label>
-        <label for="">班級座號：<input type="text" name="seat_num" id=""></label>
-        <label for="">姓名：<input type="text" name="name" id=""></label>
-        <label for="">生日<input type="text" name="birthday" id=""></label>
-        <label for="">身分證號碼<input type="text" name="national_id" id=""></label>
-        <label for="">住址<input type="text" name="address" id=""></label>
-        <label for="">家長<input type="text" name="parent" id=""></label>
-        <label for="">電話<input type="text" name="telphone" id=""></label>
-        <label for="">科別<input type="text" name="major" id=""></label>
-        <label for="">畢業國中<input type="text" name="secondary" id=""></label>
+        <label for="">學號：<input type="text" name="uni_id" value="<?=$student['uni_id'];?>"></label>
+        <label for="">班級座號：<input type="text" name="seat_num" value="<?=$student['seat_num'];?>"></label>
+        <label for="">姓名：<input type="text" name="name" value="<?=$student['name'];?>"></label>
+        <label for="">生日<input type="text" name="birthday" value="<?=$student['birthday'];?>"></label>
+        <label for="">身分證號碼<input type="text" name="national_id" value="<?=$student['national_id'];?>"></label>
+        <label for="">住址<input type="text" name="address" value="<?=$student['address'];?>"></label>
+        <label for="">家長<input type="text" name="parent" value="<?=$student['parent'];?>"></label>
+        <label for="">電話<input type="text" name="telphone" value="<?=$student['telphone'];?>"></label>
+        <label for="">科別<input type="text" name="major" value="<?=$student['major'];?>"></label>
+        <label for="">畢業國中<input type="text" name="secondary" value="<?=$student['secondary'];?>"></label>
         <input type="hidden" name="id" value="<?=$id;?>">
         <input type="submit" value="更新">
         <input type="reset" value="重置">
