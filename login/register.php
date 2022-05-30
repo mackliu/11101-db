523943ab2ddc3d784c3580cb8bfcc5a52665e730
mackliu <mackliu@gmail.com>
2022-05-30 16:26:03 +0800

    完成註冊新會員功能

--- login/register.php
+++ login/register.php
@@ -7,7 +7,33 @@
     <title>會員註冊</title>
 </head>
 <body>
-
     <h1>會員註冊</h1>
+<form action="store_member.php" method="post">
+    <table>
+        <tr>
+            <td>帳號</td>
+            <td><input type="text" name="acc" id=""></td>
+        </tr>
+        <tr>
+            <td>密碼</td>
+            <td><input type="password" name="pw" id=""></td>
+        </tr>
+        <tr>
+            <td>生日</td>
+            <td><input type="date" name="birthday" id=""></td>
+        </tr>
+        <tr>
+            <td>密碼提示</td>
+            <td><input type="text" name="passnote" id=""></td>
+        </tr>
+        <tr>
+            <td>email</td>
+            <td><input type="email" name="email" id=""></td>
+        </tr>
+    </table>
+    <div>
+        <input type="submit" value="註冊"><input type="reset" value="重置">
+    </div>
+</form>
 </body>
 </html>
---
+++ login/store_member.php
@@ -0,0 +1,14 @@
+<?php
+$dsn="mysql:host=localhost;charset=utf8;dbname=member";
+$pdo=new PDO($dsn,'root','');
+
+$acc=$_POST['acc'];
+
+$sql="INSERT INTO `users` (`acc`,`pw`,`birthday`,`passnote`,`email`) 
+                    values('{$_POST['acc']}','{$_POST['pw']}','{$_POST['birthday']}','{$_POST['passnote']}','{$_POST['email']}');";
+
+$pdo->exec($sql);
+
+header('location:login.php');
+
+?>
