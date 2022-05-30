5a9ca6834c54a8bc36b58dd8153915c166c95a06
mackliu <mackliu@gmail.com>
2022-05-30 10:49:11 +0800

    完成了新增資料寫入資料表

--- php/add.php
+++ php/add.php
@@ -19,17 +19,17 @@
 </head>
 <body>
     <h1>新增學生資料</h1>
-    <form action="" method="post">
-        <label for="">學號：<input type="text" name="" id=""></label>
-        <label for="">班級座號：<input type="text" name="" id=""></label>
-        <label for="">姓名：<input type="text" name="" id=""></label>
-        <label for="">生日<input type="text" name="" id=""></label>
-        <label for="">身分證號碼<input type="text" name="" id=""></label>
-        <label for="">住址<input type="text" name="" id=""></label>
-        <label for="">家長<input type="text" name="" id=""></label>
-        <label for="">電話<input type="text" name="" id=""></label>
-        <label for="">科別<input type="text" name="" id=""></label>
-        <label for="">畢業國中<input type="text" name="" id=""></label>
+    <form action="store.php" method="post">
+        <label for="">學號：<input type="text" name="uni_id" id=""></label>
+        <label for="">班級座號：<input type="text" name="seat_num" id=""></label>
+        <label for="">姓名：<input type="text" name="name" id=""></label>
+        <label for="">生日<input type="text" name="birthday" id=""></label>
+        <label for="">身分證號碼<input type="text" name="national_id" id=""></label>
+        <label for="">住址<input type="text" name="address" id=""></label>
+        <label for="">家長<input type="text" name="parent" id=""></label>
+        <label for="">電話<input type="text" name="telphone" id=""></label>
+        <label for="">科別<input type="text" name="major" id=""></label>
+        <label for="">畢業國中<input type="text" name="secondary" id=""></label>
         <input type="submit" value="新增">
         <input type="reset" value="重置">
     </form>
---
+++ php/store.php
@@ -0,0 +1,11 @@
+<?php
+$dsn="mysql:host=localhost;charset=utf8;dbname=school";
+$pdo=new PDO($dsn,'root','');
+
+$sql="INSERT INTO `students` (`uni_id`, `seat_num`, `name`, `birthday`, `national_id`, `address`, `parent`, `telphone`, `major`, `secondary`) 
+                       values( '{$_POST['uni_id']}', '{$_POST['seat_num']}', '{$_POST['name']}', '{$_POST['birthday']}', '{$_POST['national_id']}', '{$_POST['address']}', '{$_POST['parent']}', '{$_POST['telphone']}', '{$_POST['major']}', '{$_POST['secondary']}')";
+$pdo->query($sql);
+
+//echo $sql;
+header("location:index.php");
+?>
