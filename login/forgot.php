46fe70db718d3cc4601e188f5ed3f6bf36a32fc2
mackliu <mackliu@gmail.com>
2022-06-06 13:53:37 +0800

    完成找回密碼功能

--- login/forgot.php
+++ login/forgot.php
@@ -8,5 +8,10 @@
 </head>
 <body>
     <h1>尋回密碼</h1>
+    <form action="chk_acc.php" method="post">
+        <p>請輸入你要找回密碼的帳號:</p>
+        <input type="text" name='acc' ><br>
+        <input type="submit" value="檢查">
+    </form>
 </body>
 </html>
